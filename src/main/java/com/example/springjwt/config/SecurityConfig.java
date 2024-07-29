2af0c974eaf2b5a5e860c1589dd82493adcbfafa
bsjin1122 <kitty7579@hanmail.net>
2024-07-29 16:41:53 +0900

    13. CORS 설정 완료

--- src/main/java/com/example/springjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -1,48 +1,96 @@
 package com.example.springjwt.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import com.example.springjwt.jwt.JWTFilter;
+import com.example.springjwt.jwt.JWTUtil;
+import com.example.springjwt.jwt.LoginFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	//AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+	private final AuthenticationConfiguration authenticationConfiguration;
+	private final JWTUtil jwtUtil;
+	public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
+		this.authenticationConfiguration = authenticationConfiguration;
+		this.jwtUtil = jwtUtil;
+	}
 
-	// 비밀번호 암호화
+	//AuthenticationManager Bean 등록
 	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
 		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-		// csrf disable: 세션 방식에서는 세션이 고정이라 csrf 필수적으로 방어
-		// JWT 방식: stateless 방식, csrf 공격 방어하지 않아도 됨
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf((auth)-> auth.disable());
+			.cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
 
-		// form 로그인 방식 disable
+				@Override
+				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+					CorsConfiguration configuration = new CorsConfiguration();
+
+					configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+					configuration.setAllowedMethods(Collections.singletonList("*"));
+					configuration.setAllowCredentials(true);
+					configuration.setAllowedHeaders(Collections.singletonList("*"));
+					configuration.setMaxAge(3600L);
+
+					configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+					return configuration;
+				}
+			})));
+
+		http
+			.csrf((auth) -> auth.disable());
+
 		http
 			.formLogin((auth) -> auth.disable());
 
-		//http basic 인증 방식 disable
 		http
 			.httpBasic((auth) -> auth.disable());
 
-		// 경로별 인가 작업
 		http
-			.authorizeHttpRequests((auth)-> auth
+			.authorizeHttpRequests((auth) -> auth
 				.requestMatchers("/login", "/", "/join").permitAll()
-				.requestMatchers("/admin").hasRole("ADMIN") //admin권한 가진 사용자들만
-				.anyRequest().authenticated()); // 다른 요청들은 로그인한 사용자만 접근 가능하도록
+				.anyRequest().authenticated());
 
-		// 세션 설정
+		// JWTFilter 등록
 		http
-			.sessionManagement((session)-> session
+			.addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+		
+		//필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+		http
+			.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+		http
+			.sessionManagement((session) -> session
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 		return http.build();
--- src/main/java/com/example/springjwt/controller/MainController.java
+++ src/main/java/com/example/springjwt/controller/MainController.java
@@ -1,5 +1,11 @@
 package com.example.springjwt.controller;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +15,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
 	@GetMapping("/")
 	public String mainP(){
-		return "Main Controller";
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+		GrantedAuthority auth = iter.next();
+		String role = auth.getAuthority();
+		System.out.println("-----------------------------");
+		System.out.println(username);
+		System.out.println(role);
+		return "Main Controller --- username: " + username + ", role : " + role;
 	}
 }
